5a0758717466a20466ed2744285024f374acfc24
MattSegal <mattdsegal@gmail.com>
2016-08-20 18:26:42 +1000

    try add public key

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,17 +1,26 @@
 #! /bin/bash
 
 # copy .bashrc - stomp everything
-sudo cp ../.bashrc /etc/bash.bashrc
-sudo cp ../.bashrc /home/pi/.bashrc
-sudo cp ../.bashrc ~/.bashrc
+BASHRC=../.bashrc
+sudo cp $BASHRC /etc/bash.bashrc
+sudo cp $BASHRC /home/pi/.bashrc
+sudo cp $BASHRC ~/.bashrc
 
 # set timezone to AEST
 
 LOCALTIME=/etc/localtime
 AEST=/usr/share/zoneinfo/Australia/Victoria
 
+# Link AEST to localtime dir
 rm $LOCALTIME
 ln -s $AEST $LOCALTIME
 
-# reload bash
-bash
+# Setup ssh for easy putty access
+PI_PUBLIC_KEY=../pi_key.pub
+SSH_DIR = /.ssh
+
+sudo mkdir $SSH_DIR
+sudo mk $SSH_DIR/authorized_keys
+sudo cat $PI_PUBLIC_KEY >> $SSH_DIR/authorized_keys
+
+cat $SSH_DIR/authorized_keys
