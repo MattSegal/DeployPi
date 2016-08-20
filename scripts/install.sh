17779aea65b915f7b8d14253cd75285a83c39b8a
MattSegal <mattdsegal@gmail.com>
2016-08-20 18:16:51 +1000

    add aest

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+# Install remote desktop
+sudo apt-get install xrdp
+
+# Install Samba [1]
+sudo apt-get install samba
+
+# Install kodi
+sudo apt-get install kodi
+
+# Install script line ending conversion tool
+sudo apt-get install dos2unix
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,4 +5,13 @@ sudo cp ../.bashrc /etc/bash.bashrc
 sudo cp ../.bashrc /home/pi/.bashrc
 sudo cp ../.bashrc ~/.bashrc
 
+# set timezone to AEST
+
+LOCALTIME=/etc/localtime
+AEST=/usr/share/zoneinfo/Australia/Victoria
+
+rm $LOCALTIME
+ln -s $AEST $LOCALTIME
+
+# reload bash
 bash
